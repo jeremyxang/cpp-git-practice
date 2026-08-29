5e54a74057f6b6aa173ac5c4068ee0e2f345ef44
Jeremy <jeremyang905@gmail.com>
2026-08-28 21:08:58 -0700

    edit

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
-
+##hey
 int main() { //test comment
     std::cout << "Hello from Git!" << std::endl;
+    std::cout << "This line was added by Student B." << std::endl;
     return 0;
 }
